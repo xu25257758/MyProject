520fc60a536b7c61f62fb2535289c57f7d07ebac
xu <1815988440@qq.com>
2017-10-11 10:54:42 +0800

    新建项目

---
+++ MyProject/library/src/main/java/com/example/library/net/HttpUtil.java
@@ -0,0 +1,8 @@
+package com.example.library.net;
+
+/**
+ * Created by xuzhiqiang on 2017/10/11.
+ */
+
+public class HttpUtil {
+}
---
+++ MyProject/library/src/main/java/com/example/library/util/Util.java
@@ -0,0 +1,8 @@
+package com.example.library.util;
+
+/**
+ * Created by xuzhiqiang on 2017/10/11.
+ */
+
+public class Util {
+}
